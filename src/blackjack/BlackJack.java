a0c7067ee7ea48ae6af9cd96728944c2d4835c64
pupil <pupil@E219ws18.ivkhk.local>
2022-10-06 15:07:12 +0300

    Changes

--- src/blackjack/BlackJack.java
+++ src/blackjack/BlackJack.java
@@ -73,20 +73,22 @@ public class BlackJack {
            bet = b;
        }
     }
-
+    
  public static void main(String[] args) {
      
      boolean mainloop = true;
      ArrayList<Player> players = new ArrayList<>();
      Scanner scn = new Scanner(System.in);
      
-     int player_num;
+     int player_num, blind_size;
+     int[] blinds = new int[2];
+     int offset = 0;
      
-     
+     //START
      System.out.println("Welcome to BlackJack, made by Martin");
      
+     //Num of players
      while(true) {
-        player_num = 0;
         System.out.printf("Enter the number of players(1-4): ");
 
         try{
@@ -102,34 +104,109 @@ public class BlackJack {
             scn.nextLine();
             continue;
         }
+        finally {
+            scn.nextLine();
+        }
         if(0 < player_num && player_num < 5){
             break;
         }
         else {
             System.err.println("Wrong range! Input an integer in range from 1 to 4");
-            scn.nextLine();
         }
      }
      
+     //Blinds size 
+     while(true) {
+        System.out.printf("Enter the big blind: ");
+
+        try{
+            blind_size = scn.nextInt();
+        }
+        catch (java.util.InputMismatchException e) {
+            System.err.println("Wrong input! Enter an integer!");
+            scn.nextLine();
+            continue;
+        }
+        catch (Exception e){
+            System.err.println(e);
+            scn.nextLine();
+            continue;
+        }
+        finally {
+            scn.nextLine();
+        }
+        if(blind_size > 500){
+            System.out.println("Not enough chips to play with those blinds!");
+            continue;
+        }
+        else if(blind_size < 1){
+            System.out.println("Try positive numbers");
+            continue;
+        }
+        
+        blinds[0] = blind_size / 2;
+        blinds[1] = blind_size;
+        break;
+     }
      
     BlackJack bj = new BlackJack();
     BlackJack.Deck deck = bj.new Deck();
     for(int i = 0; i < player_num; i++){
-        System.out.println("Enter the name of player");
+        System.out.printf("Enter the name of player %d%n", i+1);
         players.add(bj.new Player(scn.nextLine()));
     }
-    BlackJack.Player p1 = bj.new Player("Martin");
     
     while(mainloop){
+        //Blinds phase
         for(int i = 0; i < player_num; i++){
+            players.get(i + offset).bet = blinds[0];
+            players.get((i + 1 + offset) % player_num).bet = blinds[1];
+        }
+            if(offset+1 == player_num){
+                offset = 0;
+            }
+        
+        //Betting phase
+        for(int i = 0; i < player_num; i++){
+            System.out.printf("%s, place a bet%n", players.get(i).name);
+            try{
+                players.get(i).bet = scn.nextInt();
+            }
+            catch(Exception e){
+                System.out.println(e);
+                i-= 1;
+                continue;
+            }
+            finally{
+                scn.nextLine();
+            }
+            //Too much?
+            if(players.get(i).bet > players.get(i).chips){
+                System.out.println("You have insufficient amount of chips!");
+                i-= 1;
+                continue;
+            }
+            //Too nothing?
+            else if(players.get(i).bet == 0){
+                i-=1;
+                System.out.println("Bet... something?");
+                continue;
+            }
+            //Too negative?
+            else if(players.get(i).bet < 0){
+                i-=1;
+                System.out.println("Try using positive numbers");
+                continue;
+            }
+            else{
+                System.out.println("Betting unexpected case");
+            }
             
+            //
         }
 
     break;
     }
-    
-    p1.hit(deck.cards.remove(0));
-    System.out.println(p1.hand);
     }
     
 }
